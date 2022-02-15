d73656bc3b6c28200dae227c117ff9b4e26ed07d
anupknairy <anupkshp@gmail.com>
2022-02-15 12:16:36 +0530

    added code

--- adobe/src/com/anup/github/dsa/CountSetBits.java
+++ adobe/src/com/anup/github/dsa/CountSetBits.java
@@ -4,5 +4,34 @@ public class CountSetBits {
 
     public static void main(String[] args) {
 
+        int n = 7;
+
+        System.out.println("Set bits in " + n + " is " + method1(n));
+        System.out.println("Set bits in " + n + " is " + method2(n));
+
+
+    }
+
+    private static int method1(int n) {
+        int count = 0;
+
+        while( n > 0 ) {
+            count = count + (n&1);
+            n = n >> 1;
+        }
+
+        return count;
+    }
+
+    private static int method2(int n)
+    {
+        int count = 0;
+        while(n > 0) {
+
+            n = n & (n-1);
+            count++;
+
+        }
+        return count;
     }
 }
